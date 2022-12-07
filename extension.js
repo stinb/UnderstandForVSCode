e296cafea08e213a62afdc380ed09963d046a192
Robby Bennett <robby@scitools.com>
2022-12-07 14:46:22 -0700

    Example of tree view

--- extension.js
+++ extension.js
@@ -1,5 +1,118 @@
 const vscode = require('vscode');
+
 const { spawn } = require('node:child_process');
+const fs        = require('node:fs');
+const path      = require('node:path');
+
+class DepNodeProvider {
+
+	#workspaceRoot;
+	#_onDidChangeTreeData;
+	onDidChangeTreeData;
+
+	constructor(workspaceRoot) {
+		this._onDidChangeTreeData = new vscode.EventEmitter()
+		this.onDidChangeTreeData = this._onDidChangeTreeData.event;
+		this.workspaceRoot = workspaceRoot;
+	}
+
+	refresh() {
+		this._onDidChangeTreeData.fire();
+	}
+
+	getTreeItem(element) {
+		return element;
+	}
+
+	getChildren(element) {
+		if (!this.workspaceRoot) {
+			vscode.window.showInformationMessage('No dependency in empty workspace');
+			return Promise.resolve([]);
+		}
+
+		if (element) {
+			return Promise.resolve(this.#getDepsInPackageJson(path.join(this.workspaceRoot, 'node_modules', element.label, 'package.json')));
+		} else {
+			const packageJsonPath = path.join(this.workspaceRoot, 'package.json');
+			if (this.#pathExists(packageJsonPath)) {
+				return Promise.resolve(this.#getDepsInPackageJson(packageJsonPath));
+			} else {
+				vscode.window.showInformationMessage('Workspace has no package.json');
+				return Promise.resolve([]);
+			}
+		}
+
+	}
+
+	/**
+	 * Given the path to package.json, read all its dependencies and devDependencies.
+	 */
+	#getDepsInPackageJson(packageJsonPath) {
+		const workspaceRoot = this.workspaceRoot;
+		if (this.#pathExists(packageJsonPath) && workspaceRoot) {
+			const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf-8'));
+
+			const toDep = (moduleName, version) => {
+				if (this.#pathExists(path.join(workspaceRoot, 'node_modules', moduleName))) {
+					return new Dependency(moduleName, version, vscode.TreeItemCollapsibleState.Collapsed);
+				} else {
+					return new Dependency(moduleName, version, vscode.TreeItemCollapsibleState.None, {
+						command: 'extension.openPackageOnNpm',
+						title: '',
+						arguments: [moduleName]
+					});
+				}
+			};
+
+			const deps = packageJson.dependencies
+				? Object.keys(packageJson.dependencies).map(dep => toDep(dep, packageJson.dependencies[dep]))
+				: [];
+			const devDeps = packageJson.devDependencies
+				? Object.keys(packageJson.devDependencies).map(dep => toDep(dep, packageJson.devDependencies[dep]))
+				: [];
+			return deps.concat(devDeps);
+		} else {
+			return [];
+		}
+	}
+
+	#pathExists(p) {
+		try {
+			fs.accessSync(p);
+		} catch (err) {
+			return false;
+		}
+
+		return true;
+	}
+}
+
+class Dependency extends vscode.TreeItem {
+
+	constructor(
+		label,
+		version,
+		collapsibleState,
+		command
+	) {
+		super(label, collapsibleState);
+
+		this.tooltip = `${this.label}-${this.version}`;
+		this.description = this.version;
+	}
+
+	iconPath = {
+		light: path.join(__filename, '..', '..', 'resources', 'light', 'dependency.svg'),
+		dark: path.join(__filename, '..', '..', 'resources', 'dark', 'dependency.svg')
+	};
+
+	contextValue = 'dependency';
+}
+
+
+
+
+
 let dbPath = null;
 let db = null;
 
@@ -28,43 +141,62 @@ async function openDatabase() {
 	if (db)
 		return true;
 
-	// const ls = spawn('ls');
-	// console.log(ls);
-	// ls.stdout.on('data', (data) => {
-	// 	console.log(`stdout: ${data}`);
+	// // Open
+	// db = spawn(`undjs ${dbPath}`);
+
+	// // Events
+	// db.on('disconnect', err => {
+	// 	error(`Disconnected from ${dbPath}`);
 	// });
-
-	// Open
-	db = spawn(`undjs ${dbPath}`);
-
-	// Events
-	db.on('disconnect', err => {
-		error(`Disconnected from ${dbPath}`);
-	});
-	db.on('error', err => {
-		error(`Error with ${dbPath}`);
-	});
-	db.on('exit', err => {
-		error(`Exited from ${dbPath}`);
-	});
-	db.on('message', err => {
-		info('Message from db');
-	});
-	db.on('spawn', err => {
-		error(`Opened ${dbPath}`);
-	});
+	// db.on('error', err => {
+	// 	error(`Error with ${dbPath}`);
+	// });
+	// db.on('exit', err => {
+	// 	error(`Exited from ${dbPath}`);
+	// });
+	// db.on('message', err => {
+	// 	info('Message from db');
+	// });
+	// db.on('spawn', err => {
+	// 	error(`Opened ${dbPath}`);
+	// });
 }
 
 async function getReferences(query) {
-	// No references if the database isn't open
-	if (!openDatabase())
-		return [];
+	// // No references if the database isn't open
+	// if (!(await openDatabase()))
+	// 	return {};
 
-
+	return {
+		145556: {
+			done: false,
+			name: 'setup_cpu_local_masks',
+			refs: [
+				{
+					done: false,
+					type: 'Definein',
+					file: 'C:/Users/Robby/Desktop/linuxKernel/linux-5.3.1/arch/x86/kernel/cpu/common.c',
+					line: 81,
+					column: 12,
+				},
+				{
+					done: false,
+					type: 'Declarein',
+					file: 'C:/Users/Robby/Desktop/linuxKernel/linux-5.3.1/arch/x86/include/asm/cpumask.h',
+					line: 12,
+					column: 12,
+				},
+			],
+		},
+	};
 }
 
-function seeReferences(entities) {
+function seeReferences(ents) {
+	for (const id in ents) {
+		const ent = ents[id];
 
+		// for (const ref in ent.refs)
+	}
 }
 
 
@@ -99,12 +231,12 @@ async function selectDatabase() {
 	openDatabase();
 }
 
-function seeReferencesForFile() {
-	seeReferences(getReferences(''));
+async function seeReferencesForFile() {
+	seeReferences(await getReferences(''));
 }
 
-function seeReferencesForSelected() {
-	seeReferences(getReferences(''));
+async function seeReferencesForSelected() {
+	seeReferences(await getReferences(''));
 }
 
 
@@ -115,6 +247,10 @@ function seeReferencesForSelected() {
 
 function activate(context) {
 	// openDatabase();
+
+	// Tree view
+	const nodeDependenciesProvider = new DepNodeProvider(vscode.workspace.rootPath)
+	vscode.window.registerTreeDataProvider('referenceChecklist', nodeDependenciesProvider);
 
 	// Register commands created in package.json
 	context.subscriptions.push(
